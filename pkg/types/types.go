fb757b151a7dd0a5c8dcf7192d989d78107f7454
Qadriddin <qadrdin81@gmail.com>
2021-04-06 09:54:45 +0300

    добавлены pkg

---
+++ pkg/types/types.go
@@ -0,0 +1,54 @@
+package types
+
+
+type Money int64
+
+
+type Currency string
+
+
+const (
+	TJS = "TJS"
+	RUB = "RUB"
+	USD = "USD"
+)
+
+
+type PAN string
+
+type Card struct {
+	ID       int
+	PAN      PAN
+	Balance  Money
+	Currency Currency
+	Color    string
+	Name     string
+	Active   bool
+}
+
+
+type Category string
+
+
+type Status string
+
+
+const (
+	StatusOk         Status = "OK"
+	StatusFail       Status = "FAIL"
+	StatusInProgress Status = "INPROGRESS"
+)
+
+
+type Payment struct {
+	ID       int
+	Amount   Money
+	Category Category
+	Status   Status
+}
+
+type PaymentSource struct {
+	Type    string 
+	Number  string 
+	Balance Money
+}
